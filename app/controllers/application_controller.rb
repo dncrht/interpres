1db5312f9d864d6a2403733cff2d013b8d327e96
Dani <daniel.cr.ht@gmail.com>
2014-08-02 10:06:55 +0100

    Basic authentication for the web interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :available_apps
+  before_action :basic_auth, :available_apps
 
   private
+
+  def basic_auth
+    return if ENV['HTTP_USER'].blank?
+
+    authenticate_or_request_with_http_basic do |user, password|
+      user == ENV['HTTP_USER'] && password == ENV['HTTP_PASSWORD']
+    end
+  end
 
   def available_apps
     @apps = App.all
---
+++ spec/requests/authentication_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe ApplicationController do
+
+  context 'without auth' do
+    it 'goes to root path' do
+      ENV['HTTP_USER'] = nil
+
+      get apps_path
+
+      expect(response).to be_success
+    end
+  end
+
+  context 'with auth' do
+
+    it 'throws error with wrong credentials' do
+      ENV['HTTP_USER'] = 'wrong'
+
+      get apps_path
+
+      expect(response.code).to eq '401'
+    end
+
+    it 'goes to root path' do
+      ENV['HTTP_USER'] = 'right'
+      ENV['HTTP_PASSWORD'] = '123456'
+      basic_auth = ActionController::HttpAuthentication::Basic.encode_credentials ENV['HTTP_USER'], ENV['HTTP_PASSWORD']
+
+      get apps_path, {}, {'HTTP_AUTHORIZATION' => basic_auth}
+
+      expect(response).to be_success
+    end
+  end
+end
